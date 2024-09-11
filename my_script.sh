667277bfa78a10a600a01e47b1cbeb0dfc61ba0c
Bishwas Adhikari <147196562+bishwasadh@users.noreply.github.com>
2024-09-11 19:19:25 -0400

    Add script to create, write, display, and change permissions of activity.txt

---
+++ my_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Step 2: Create a new file called activity.txt
+touch activity.txt
+
+# Step 3: Write text into activity.txt
+echo "Hello from the shell!" > activity.txt
+
+# Step 4: Display the content of activity.txt
+cat activity.txt
+
+# Step 5: Change the file's permissions to be executable
+chmod +x activity.txt
